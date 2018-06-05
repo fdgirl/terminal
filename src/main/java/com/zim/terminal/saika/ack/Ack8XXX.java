d95281d057d86bd2d287e06774b94ffcc6621b18
unknown <285699686@qq.com>
2018-06-05 16:54:18 +0800

    8008-8011

--- src/main/java/com/zim/terminal/saika/ack/Ack8XXX.java
+++ src/main/java/com/zim/terminal/saika/ack/Ack8XXX.java
@@ -346,7 +346,7 @@ public class Ack8XXX {
         }
         byte[] _terminal = FormatUtils.strToByte(tsb.toString());
         byte sn = FormatUtils.getSN();
-        byte[] cmd = new byte[]{(byte) 0x80, 0x08};
+        byte[] cmd = new byte[]{(byte) 0x80, 0x11};
         
         StringBuffer secretStr = new StringBuffer(productKey);
         secretStr.append(secret);
